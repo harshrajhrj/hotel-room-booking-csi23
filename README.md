6d934f851ee946fcce7edde07fb195cad99e0aa2
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-07-12 15:24:04 +0530

    configured express session and session storage

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ npm install --save multer
 ```javascript
 npm install --save multer-gridfs-storage
 ```
+### Flash messages
+* Store flash messages in session<sup>[Link](https://github.com/jaredhanson/connect-flash)</sup>
+```javascript
+npm install connect-flash
+```
+### Express session
+* Creates a session and stores session data at server-side, be it a database storage or default session storage `MemoryStore`(not preferred)<sup>[Link](https://expressjs.com/en/resources/middleware/session.html)</sup>
+```javascript
+npm install express-session
+```
+
+### Mongo storage for express session
+* Create a mongo storage to store express session<sup>[Link](https://github.com/jdesboeufs/connect-mongo)</sup>
+```javascript
+npm install connect-mongo
+```
--- app.js
+++ app.js
@@ -1,7 +1,50 @@
-const express = require('express');
 const ConnectDB = require('./ConnectDB');
 ConnectDB();
+const express = require('express');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
+const path = require('path');
+const mongoSession = require('connect-mongo');
 const app = express();
+
+// middleware to keep the incoming objects in JSON format
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// creating a session middleware
+app.use(expressSession({
+    secret: 'hotel server session',
+    cookie: {
+        maxAge: 60000 * 60 * 24
+    },
+    saveUninitialized: false,
+    name: 'hotel room booking',
+    resave: false,
+
+    // using MongoDB based session storage
+    store: mongoSession.create({
+        mongoUrl: process.env.DB_URL,
+        autoRemove: 'interval',
+        autoRemoveInterval: 10
+    })
+}))
+
+// flash middleware to store text messages in session
+app.use(flash());
+
+// type of flash messages
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+// view engine set to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// middleware to server static files with express
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', async (req, res) => {
     res.send('Hello !');
