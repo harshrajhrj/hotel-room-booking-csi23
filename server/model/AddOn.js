9b81c4f5a3ffe6b24bfae36a81c99625218c20c7
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-07-24 22:39:26 +0530

    started adding comments

--- server/model/AddOn.js
+++ server/model/AddOn.js
@@ -16,6 +16,9 @@ AddOnSchema.pre('save', calcAddOnPrice);
 
 AddOnSchema.pre('findOneAndUpdate', calcAddOnPrice);
 
+/**
+ * This function calculatues the prices of given add-ons.
+ */
 function calcAddOnPrice() {
     const AddOnPriceMapping = new Map([['Quiet location', '20'], ['Luxury linens', '35 '], ['Breakfast', '40'], ['Fresh flowers', '40'], ['Champagne & Chocolate', '60'], ['Local attraction ticket', '15']]);
     this.price = AddOnPriceMapping.get(this.type);
